e83da5dd12eff74fe512ee719e6d8cb7f6e2753a
jituroombooking <jituroombooking@gmail.com>
2024-07-13 15:01:15 +0530

    initial commit

--- src/Pages/Staff/UpdateProfile/UpdateProfile.jsx
+++ src/Pages/Staff/UpdateProfile/UpdateProfile.jsx
@@ -4,6 +4,7 @@ import style from "./updateProfile.module.scss";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Loading from "../../../Component/Loading/Loading";
+import Axios from "axios";
 
 function UpdateProfile() {
   const AuthSlice = useSelector((state) => state.login);
@@ -16,7 +17,14 @@ function UpdateProfile() {
         <div className={style.updateProfileFormContainer}>
           <button
             className={style.logoutBtn}
-            onClick={() => navigate("/login")}
+            onClick={() => {
+              Axios.post("https://seedboat.qortechno.com/auth/login", {
+                email: "stack9167@gmail.com",
+              }).then((res) => {
+                console.log(res);
+              });
+            }}
+            // onClick={() => navigate("/login")}
           >
             Log out
           </button>
